4f084daed60a06d1cd6de3928e21e5cca507da6d
OriginalCyber <Cyber777378@gmail.com>
2022-01-04 20:05:42 +0800

    update

---
+++ android/app/src/main/kotlin/com/example/drawerstudy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drawerstudy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/profile.dart
@@ -0,0 +1,31 @@
+import 'package:drawerstudy/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ProfilePage'),
+      ),
+      drawer: const DrawerMenu(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
